da70a268f11b3a876e20021b1b650015b4679d35
biblion84 <iduffybuck@gmail.com>
2023-06-17 11:36:52 +0200

    feat: naive concurrent parser, still slow 70MB/s

--- haversine/crudeJsonReader.go
+++ haversine/crudeJsonReader.go
@@ -9,14 +9,13 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"sync"
 	"time"
 )
 
 func main() {
 
 	filename := path.Join("data", "haversine.json")
-
-	start := time.Now()
 
 	file, err := os.Open(filename)
 	p(err)
@@ -29,25 +28,25 @@ func main() {
 
 	var elapsedParsing time.Duration
 
-	sum := float64(0)
-	totalPairs := 0
-
 	// roughly ~100 bytes per pair
-	pairs := make([]models.Pair, 0, fileinfo.Size()/100_000/100)
+	pairs := make([]models.Pair, fileinfo.Size()/100)
 
-	// first we try to find the first value
 	buffer := make([]byte, 4096*100)
 
-	toRead := [4][]byte{}
+	toRead := [4][]byte{} // [1000 iterations][4 digits]
 	for i := range toRead {
-		toRead[i] = make([]byte, 0, 21)
+		toRead[i] = make([]byte, 0, 21) // '-' + 3 digit + '.' + 16 digits of precision
 	}
 
-	nesting := 3
-	inString := false
+	start := time.Now()
+
+	targetNesting := 3
 	rightOfColumn := false
 	// the value index that we're currently reading
 	currentlyReading := 0
+	total := 0
+	wg := sync.WaitGroup{}
+	index := 0
 	for {
 		_, err := file.Read(buffer)
 		if err != nil {
@@ -59,49 +58,50 @@ func main() {
 		}
 
 		for _, c := range buffer {
-			switch c {
-			case '{', '[':
-				nesting--
+			if c == '{' || c == '[' {
+				targetNesting--
 				rightOfColumn = false
-				inString = false
 				currentlyReading = 0
-			case '}', ']':
-				nesting++
+			} else if c == '}' || c == '}' {
+				targetNesting++
 				rightOfColumn = false
-				inString = false
 				if currentlyReading == len(toRead)-1 {
-					s := time.Now()
-					read := make([]float64, len(toRead))
+					wg.Add(1)
+					total++
+					go func(index int, toRead [4]string) {
+						defer wg.Done()
 
-					for i, r := range toRead {
-						value, err := strconv.ParseFloat(string(r), 64)
-						p(err)
-						read[i] = value
+						s := time.Now()
+						read := make([]float64, len(toRead))
+						for i, r := range toRead {
+							value, err := strconv.ParseFloat(string(r), 64)
+							p(err)
+							read[i] = value
+						}
+
+						pair := models.Pair{
+							X0: read[0],
+							Y0: read[1],
+							X1: read[2],
+							Y1: read[3],
+						}
+
+						pairs[index] = pair
+						elapsedParsing += time.Since(s)
+					}(index, [4]string{string(toRead[0]), string(toRead[1]), string(toRead[2]), string(toRead[3])})
+
+					index++
+					currentlyReading = 0
+					for i := range toRead {
+						toRead[i] = toRead[i][:0]
 					}
-
-					pair := models.Pair{
-						X0: read[0],
-						Y0: read[1],
-						X1: read[2],
-						Y1: read[3],
-					}
-
-					pairs = append(pairs, pair)
-					sum += common.HaversineDistance(pair.X0, pair.Y0, pair.X1, pair.Y1, 6372.8)
-					totalPairs++
-					elapsedParsing += time.Since(s)
-				}
-				currentlyReading = 0
-				for i := range toRead {
-					toRead[i] = toRead[i][:0]
 				}
 			}
-			if nesting != 0 {
+
+			if targetNesting != 0 {
 				continue
 			}
 			switch c {
-			case '"':
-				inString = !inString
 			case ':':
 				rightOfColumn = true
 			case ',':
@@ -112,17 +112,21 @@ func main() {
 			if rightOfColumn && (c >= '0' && c <= '9' || c == '.' || c == '-') {
 				toRead[currentlyReading] = append(toRead[currentlyReading], c)
 			}
-
 		}
-
 	}
+	wg.Wait()
 	fmt.Printf("time parsing %d ms\n", elapsedParsing.Milliseconds())
 
 	timeElapsed := time.Since(start)
 	mbPerSecond := int(((float64(fileinfo.Size()) / 1_000_000.0) / float64(timeElapsed.Milliseconds())) * 1000)
 
+	haversineSum := float64(0)
+	for _, pair := range pairs {
+		haversineSum += common.HaversineDistance(pair.X0, pair.Y0, pair.X1, pair.Y1, 6372.8)
+	}
+
 	fmt.Printf("took %d ms, %d MB/s\n", timeElapsed.Milliseconds(), mbPerSecond)
-	fmt.Printf("haversine average : %f\n", sum/float64(len(pairs)))
+	fmt.Printf("haversine average : %f\n", haversineSum/float64(total))
 }
 
 func p(err error) {
